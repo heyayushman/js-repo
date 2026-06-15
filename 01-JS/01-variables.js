1453bc05d4c05c3cb61ca16df6280400594c0e65
Ayushman Baitha <ayushmanbaitha4@gmail.com>
2026-06-15 19:41:22 +0000

    read me and variables done

---
+++ 01-JS/01-variables.js
@@ -0,0 +1,21 @@
+const accountId = 525865;
+let accountEmail = "hello@google.com";
+var accountPassword = "123456";
+accountCity = "Agra";
+let accountState;
+// accountId = 58 // can not be changed, this is not allowed
+
+// console.log(accountEmail);
+
+accountEmail = "hi@hello.com";
+// console.log(accountEmail);
+
+accountPassword = "password@569875";
+// console.log(accountPassword);
+
+accountCity = "Bengaluru";
+// console.log(accountCity);
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # js-repo
 A code repo for JavaScript
+
+#const - It is used when the value can not be changed
+#let - it is also used to declare variable in JS, it has lexical scope
+#var - it is used to declare variable in JS, it has global scope
+
+#To print variables to the console:
+*1. console.log() : it will show only one variable
+*2. console.table([varibales name]) : it shows all the values of variables in a tabular form
+![alt text](image.png)
