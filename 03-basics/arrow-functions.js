cc59754c17864d9acb5786335de2c7b5880e079d
Ayushman Baitha <ayushmanbaitha4@gmail.com>
2026-07-16 10:42:14 +0530

    this and arrow

--- 03-basics/arrow-functions.js
+++ 03-basics/arrow-functions.js
@@ -1,9 +1,22 @@
 const user = {
     username: "Kristine",
     price: 999,
-    welcomeMessage: function(){
-        console.log(`Hello ${this.username} welcome to our platform, pay the fee of rupees ${this.price}`);
+    welcomeMessage: function() {
+        console.log(`Hello ${this.username} welcome to our platform, pay the fee of rupees ${this.price} and access special features.`);
+        console.log(this);
     }
-}
+    
+}// this keyword refers to the object where the properties belong to
 
-user.welcomeMessage();
+// user.welcomeMessage();
+// user.username = "Leo";
+// user.welcomeMessage();
+
+// console.log(this);
+
+
+function hello(){
+    let username = "Harry";
+    console.log(this.username);
+}
+hello();
