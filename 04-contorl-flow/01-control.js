cdfda67968910a9094bd08a6ff78ec1fb07414cb
Ayushman Baitha <ayushmanbaitha4@gmail.com>
2026-07-19 00:32:42 +0530

    control flow in JS

--- 04-contorl-flow/01-control.js
+++ 04-contorl-flow/01-control.js
@@ -3,15 +3,33 @@
 let age = 15;
 let voterID = false;
 
-if(age >= 18 || voterID){
+// if(age >= 18 || voterID){
    
-    if(voterID){
-        console.log("Eligible for vote");
-    }else {
-        console.log("Apply for voterID");
-    }
-}else {
-    console.log("Under age...! Come after 18 years of age..!");
+//     if(voterID){
+//         console.log("Eligible for vote");
+//     }else {
+//         console.log("Apply for voterID");
+//     }
+// }else {
+//     console.log("Under age...! Come after 18 years of age..!");
+// }
+
+
+
+// const score = 200;
+// let power;
+// if(score > 100){
+//     power = "fly";
+//     console.log(`User power: ${power}`);
+// }
+
+const userLoggedIn = true;
+const debitCard = null;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if(userLoggedIn && debitCard && (loggedInFromGoogle || loggedInFromEmail)){
+    console.log("Welcome, You are allowed to buy courses")
+}else{
+    console.log("You don't have a debit card..!")
 }
-
-
