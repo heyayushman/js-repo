bc62e5d5390a828ba6feec1a885c00786774f444
Ayushman Baitha <ayushmanbaitha4@gmail.com>
2026-07-19 00:00:27 +0530

    control flow in js

---
+++ 04-contorl-flow/01-control.js
@@ -0,0 +1,17 @@
+// control flow - 
+
+let age = 15;
+let voterID = false;
+
+if(age >= 18 || voterID){
+   
+    if(voterID){
+        console.log("Eligible for vote");
+    }else {
+        console.log("Apply for voterID");
+    }
+}else {
+    console.log("Under age...! Come after 18 years of age..!");
+}
+
+
