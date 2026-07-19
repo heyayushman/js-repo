55575ccb3f145e17f248b76c30d687f2b5c57001
Ayushman Baitha <ayushmanbaitha4@gmail.com>
2026-07-19 23:54:20 +0530

    for in loop in JS

--- 05-iteration/for-in-loop.js
+++ 05-iteration/for-in-loop.js
@@ -1,23 +1,43 @@
 //? for..in : Helps iterating in Objects
 const myObj = {
-    JS: "JavaScript",
-    py: "Python",
-    Rb: "Ruby",
-    cpp: "C++"
-    
-}
+  JS: "JavaScript",
+  py: "Python",
+  Rb: "Ruby",
+  cpp: "C++",
+};
 
 // for(const key in myObj){
 //     console.log(`${key}: ${myObj[key]}`);
 // }
 
-
-const myArr = ["JS", "Java","C++", "Python"];
-for(const val in myArr){
-    // console.log(myArr[val]);
+const myArr = ["JS", "Java", "C++", "Python"];
+for (const val in myArr) {
+  // console.log(myArr[val]);
 }
 
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+  city: "Bangalore",
+};
 
+// for (const key in person) {
+//   console.log(`${key}: ${person[key]}`);
+// }
+
+
+//? for..of cannot be used in Objects directly, instead we can convert an object into an array by using Object methods such Object.keys(), Object.values(), Object.entries() etc
+
+// const personKeys = Object.keys(person);
+// const personValues = Object.values(person);
+// const personEntries = Object.entries(person);
+
+// console.log(personEntries);
+// for(const key of personKeys){
+//     console.log(key,": " , person[key]);
+// }
 // const map = new Map();
 // map.set('IN', "India");
 // map.set('USA', "United States Of America");
