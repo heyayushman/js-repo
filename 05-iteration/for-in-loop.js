d29f9cb8ebc461ca69cbee96dddaacbeb5e494a5
Ayushman Baitha <ayushmanbaitha4@gmail.com>
2026-07-19 22:57:05 +0530

    for..in loop in JS

---
+++ 05-iteration/for-in-loop.js
@@ -0,0 +1,30 @@
+//? for..in : Helps iterating in Objects
+const myObj = {
+    JS: "JavaScript",
+    py: "Python",
+    Rb: "Ruby",
+    cpp: "C++"
+    
+}
+
+// for(const key in myObj){
+//     console.log(`${key}: ${myObj[key]}`);
+// }
+
+
+const myArr = ["JS", "Java","C++", "Python"];
+for(const val in myArr){
+    // console.log(myArr[val]);
+}
+
+
+// const map = new Map();
+// map.set('IN', "India");
+// map.set('USA', "United States Of America");
+// map.set('JPN', "Japan");
+// map.set('FR', "France");
+// map.set('JPN', "Japan");
+
+// for(const key in map){
+//     console.log(key);
+// }
