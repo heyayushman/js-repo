63403d5c0175996febe25d0c67f98fe995bc7d56
Ayushman Baitha <ayushmanbaitha4@gmail.com>
2026-07-21 20:51:47 +0530

    changes in for each file

--- 05-iteration/for-each-loop.js
+++ 05-iteration/for-each-loop.js
@@ -2,35 +2,8 @@
 
 const myArr = ["JS", "Java", "C++", "Python"];
 
-// myArr.forEach((item) => {
-//     console.log(item);
-// })
-
-// function printItem(item){
-//     console.log(item);
-// }
-
-// myArr.forEach(printItem);
-
-// myArr.forEach((item, index, array) => {
-//     console.log(item, index, array);
-// })
-
-const programmingLanguage = [
-  {
-    languageName: "JavaScript",
-    languageFileName: "js",
-  },
-  {
-    languageName: "Java",
-    languageFileName: "java",
-  },
-  {
-    languageName: "Python",
-    languageFileName: "py",
-  }
-];
-
-programmingLanguage.forEach((element) => {
-    console.log(element.languageName);
+const lang = myArr.forEach((item) => {
+  console.log(item);
 })
+
+console.log(lang);
